77562e9b12b37514c345d0c172a861808e498949
Tinh Phan <pttinh0804@gmail.com>
2025-03-23 18:28:54 +0700

    style: setup layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="bg-white px-12">{children}</div>
       </body>
     </html>
   );
 }
+
